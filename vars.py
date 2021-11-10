c707e67d90d5ab8a28e2c4d03b0244c9f9c179bb
Shadabshaikh07 <shadab.biscuitwala@gmail.com>
2021-11-10 14:31:07 +0100

    Number And Types

---
+++ vars.py
@@ -0,0 +1,58 @@
+a = 257293579275927597125972589723489578957679676813460813468234609238902578345723458234
+print(type(a))
+b = 10
+c = a + b
+print('a: {}'.format(a))
+print('c: {}'.format(c))
+
+d = 6347347437473474747.635645326
+print(type(d))
+e = 10.0
+print(type(e))
+f = d + e
+print('f: {}'.format(f))
+
+
+max_int = 1.8e380
+
+print(max_int)
+
+bb = 0b101
+print(bb)
+xx=0x101
+print(xx)
+xx=0xf1
+print(xx)
+
+
+a1= 1349
+b1= 353
+c1 = a1 / b1
+print('c1: {}'.format(c1))
+print('c1: ' + str(c1))
+
+print(a1)
+
+c2 = int(c1)
+print('c2: {}'.format(c2))
+
+c3 = round(c1, 2)
+print('c3: {}'.format(c3))
+
+s = '34644894605.25116895'
+print('type of s: {}'.format(type(s)))
+f = float(s)
+print(type(f))
+print(f)
+
+s = '848916177189181'
+print('--------------')
+print('s is digit: %s' % s.isdigit())
+print('s is numeric: %s' % s.isnumeric())
+print('s is alpha: %s' % s.isalpha())
+print(s)
+print('type of s: {}'.format(type(s)))
+print('--------------------')
+
+s1 = '101'
+print(s1)
