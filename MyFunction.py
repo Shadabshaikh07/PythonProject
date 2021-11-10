7399bbb9a4d859a5857e58d4a78bc335f5163af2
Shadabshaikh07 <shadab.biscuitwala@gmail.com>
2021-11-10 14:43:08 +0100

    String Operations

--- MyFunction.py
+++ MyFunction.py
@@ -1,14 +1,5 @@
 if __name__ == '__main__':
-     s = 'abcdfghijklmanopqrstuvwxyz'
-     print(len(s))
-     print(s[1:5])
-     # first 7 letters
-     print(s[:7])
-     # last 7 letters
-     print(s[~7:])
-     print(s[~7:-4])
-     print(s.count('a'))
-     print(s.index('a'))
-     print(s.rindex('a'))
 
 
+    # Use a breakpoint in the code line below to debug your script.
+    print_hi('PyCharm')
---
+++ strings.py
@@ -0,0 +1,11 @@
+s = 'abcdfghijklmanopqrstuvwxyz'
+print(len(s))
+print(s[1:5])
+# first 7 letters
+print(s[:7])
+# last 7 letters
+print(s[~7:])
+print(s[~7:-4])
+print(s.count('a'))
+print(s.index('a'))
+print(s.rindex('a'))
