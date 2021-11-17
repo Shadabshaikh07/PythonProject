c9d9172fcfe430fd9912ee772f3d3d0446136381
Shadabshaikh07 <shadab.biscuitwala@gmail.com>
2021-11-17 14:36:21 +0100

    Loops

--- Loops.py
+++ Loops.py
@@ -2,9 +2,21 @@ for i in range(8):
     print(i)
 
 print('-------------')
+x=10
 for i in range(8):
     if i==3:
         continue
     if i==6:
         break
     print(i)
+
+print('------------')
+for i in range(1, x+1):
+    print(i)
+
+
+print('------------')
+a = 1
+while a < 17:
+    print(a)
+    a = a + a
