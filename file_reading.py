aead45e5211e2cff3dad332ae5e617554bb3e71e
Shadabshaikh07 <shadab.biscuitwala@gmail.com>
2021-11-24 13:49:04 +0100

    File Reading

---
+++ file_reading.py
@@ -0,0 +1,19 @@
+file_path = 'test.txt'
+
+with open(file_path, 'r') as my_file:
+    lines = my_file.readlines()
+    i = 1
+    for line in lines:
+        print('{}: {}'.format(i,line), end='')
+        i+=1
+
+
+# file_path = 'test.txt'
+
+# my_file = open(file_path, 'r')
+# lines = my_file.readlines()
+# i = 0
+# for line in lines:
+#     print('{}: {}'.format(i,line), end='')
+#     i+=1
+# my_file.close()
