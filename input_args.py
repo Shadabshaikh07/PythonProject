c688fc9ef0bd606d78ba3dffd70b3f3be90d12fe
Shadabshaikh07 <shadab.biscuitwala@gmail.com>
2021-11-24 14:09:18 +0100

    File Writting and Input Arguments

--- input_args.py
+++ input_args.py
@@ -2,3 +2,10 @@ import sys
 
 if __name__ == '__main__':
     print(sys.argv)
+    for arg in sys.argv:
+        print(arg)
+        print(type(arg))
+
+
+    # print(sys.argv[1])
+    # print(type(sys.argv[1]))
