1b78551118fcdcc7a902714c4574ed1fb609efb9
Shadabshaikh07 <shadab.biscuitwala@gmail.com>
2021-12-01 15:07:51 +0100

    Loops(List) Ex

--- file writting.py
+++ file writting.py
@@ -1,10 +1,10 @@
 file_path = 'test_out.txt'
 
-# with open(file_path, 'a') as my_file:
-#     my_file.write('First lines\n')
-#     my_file.write('Second line\n')
+with open(file_path, 'a') as my_file:
+    my_file.write('First lines\n')
+    my_file.write('Second line\n')
 
 
-my_file = open(file_path, 'w')
-my_file.write('First lines\n')
-my_file.write('Second line\n')
+# my_file = open(file_path, 'w')
+# my_file.write('First lines\n')
+# my_file.write('Second line\n')
--- list_2D.py
+++ list_2D.py
@@ -1,2 +1,42 @@
-l2d = [[5, 5, 7, ], [9, 6, 4], [9,14, 4]]
+l2d = [[5, 5, 7],
+       [9, 6, 4],
+       [9,14, 4],
+       [1, 4, 5]]
 print(l2d[2][1])
+
+#TODO
+# 1. sum the rows of l2d: [17, 19, 27, 10]
+# 2. sum of columns of l2d: [24, 29, 20]
+# 3. sum all elements of l2d
+
+
+print('--------------')
+
+l2d = [[5, 5, 7],
+        [9, 6, 4],
+        [9, 14, 4],
+        [1, 4, 5]
+        ]
+su = [sum(i) for i in l2d]
+print(su)
+
+print('--------------------')
+
+# colsum = []
+# for i in range(len(l2d)):
+#     sum = 0
+#     for j in range(len(l2d)):
+#         sum=sum + l2d[j][i]
+#     print("Sum of columns {}:".format(i+1), (sum))
+#     colsum.append(sum)
+#
+# print(colsum)
+
+print('---------')
+
+import numpy as np
+s_rows = np.sum(l2d, axis=1)
+print(s_rows)
+
+s_cols = np.sum(l2d, axis=0)
+print(s_cols)
