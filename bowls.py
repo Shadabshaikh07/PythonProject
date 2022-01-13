e6cf20b629529aa9e47abc221d5e3b2957ee18b2
Mohamedshadab Biscuitwala <92030623+Shadabshaikh07@users.noreply.github.com>
2022-01-13 03:39:43 +0100

    Bowls solution

--- bowls.py
+++ bowls.py
@@ -1,11 +1,24 @@
-n = 10
+def bowls_recursive(n):
+    if n == 1:
+        return 1
+    else:
+        return bowls_recursive(n-1) + n
 
 def bowls_loop(n):
     sum = 0
-    #TODO...
+    for i in range(n + 1):
+        sum = sum + i
     return sum
 
 
 def bowls_sequence(n):
-     #TODO ...
-    return n
+     sum = n * (n+1)/2
+     return int(sum)
+
+
+print("Enter the rows :")
+limit = input()
+bowl = 0
+# print('Bowls: {}'.format(bowls_loop(int(limit))))
+print('Bowls: {}'.format(bowls_sequence(int(limit))))
+# print('Bowls: {}'.format(bowls_recursive(int(limit))))
