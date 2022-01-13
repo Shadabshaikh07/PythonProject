b21f498cc744396d811516dfa5c213d593c15c27
Mohamedshadab Biscuitwala <92030623+Shadabshaikh07@users.noreply.github.com>
2022-01-13 04:01:10 +0100

    Space count

--- space_count.py
+++ space_count.py
@@ -1,15 +1,17 @@
 import os
 
-my_dir = 'c:\\Users\\Shadab'
+my_dir = 'C:\\Users\\Shadab\\OneDrive\\Desktop\\IMDB'
 
 
 def sum_space(folder):
     sum = 0
-    for f in os.listdir(my_dir):
+    for f in os.listdir(folder):
         # type = 'd' if os.path.isdir(os.path.join(my_dir, f)) else '-'
-        if os.path.isfile(os.path.join(my_dir, f)):
-            size = os.path.getsize(os.path.join(my_dir, f))
+        if os.path.isfile(os.path.join(folder, f)):
+            size = os.path.getsize(os.path.join(folder, f))
             sum = sum + size
+        else:
+            sum += sum_space(os.path.join(folder, f))
     return sum
 
 
